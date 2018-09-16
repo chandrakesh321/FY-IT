d0d71e548252be6d52c95af22e2f7341f814b236
chandrakesh321 <chandrakeshmungilwa123@gmail.com>
2018-09-15 22:29:36 -0700

    chandrakesh.sh file

---
+++ chandrakesh.sh
@@ -0,0 +1,12 @@
+#!/bin/base
+echo "Making foss folder"
+mkdir foss
+echo "Going to foss folder"
+cd foss
+echo "creating test_file.txt"
+touch test_file.txt
+echo""
+echo "showing output of ls:"
+ls
+echo""
+echo"updating linux repos"
